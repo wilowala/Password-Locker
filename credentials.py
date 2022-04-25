18a35ce89574a31762fc05359f99ae52a96be06d
William Owalla <william.owalla@student.moringaschool.com>
2022-04-25 10:57:30 +0300

    instanciate user account

--- credentials.py
+++ credentials.py
@@ -6,7 +6,7 @@ class UserCredentials:
     credentials=[]
 
     @classmethod
-    def userVerification(cls,u_name,l_password):
+    def userVerification(cls, u_name, l_password):
        
         _user = " "
         for user in UserAccount.userAccounts:
@@ -14,4 +14,8 @@ class UserCredentials:
                 _user == user.u_name
         return _user
 
-    
+    def __init__(self,myAccount, u_name, l_password):
+        '
+        self.myAccount = myAccount        
+        self.u_name = u_name
+        self.l_password = l_password
