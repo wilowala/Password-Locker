2ab3b101c5542f0a8116fe674db5a697e79caa9b
William Owalla <william.owalla@student.moringaschool.com>
2022-04-25 10:55:05 +0300

    add user verification method

--- credentials.py
+++ credentials.py
@@ -5,4 +5,13 @@ class UserCredentials:
 
     credentials=[]
 
+    @classmethod
+    def userVerification(cls,u_name,l_password):
+       
+        _user = " "
+        for user in UserAccount.userAccounts:
+            if user.u_name == u_name and user.l_password ==l_password:
+                _user == user.u_name
+        return _user
+
     
