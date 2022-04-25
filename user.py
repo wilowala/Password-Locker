5edbbd113f377fcb6b90a629960c70ab389f84ed
William Owalla <william.owalla@student.moringaschool.com>
2022-04-25 10:33:05 +0300

    ft: create an instance of UserAccount

---
+++ user.py
@@ -0,0 +1,12 @@
+class UserAccount:
+
+    userAccounts=[]
+
+    def __init__(self,f_name,m_name,l_name,u_name,u_email,l_password ):
+        
+        self.f_name = f_name 
+        self.m_name = m_name 
+        self.l_name = l_name 
+        self.u_name = u_name 
+        self.u_email = u_email
+        self.l_password = l_password 
