800c54913be2b87e95c2409aab95f36d0c47d298
William Owalla <william.owalla@student.moringaschool.com>
2022-04-25 11:35:15 +0300

    add save and dispaly user account's methods

--- launch.py
+++ launch.py
@@ -2,6 +2,16 @@ from user import UserAccount
 from  credentials import  UserCredentials
 
 def createNewUserAccount(f_name, m_name, l_name, u_name, u_email, l_password):
-    
+
     newUserAccount = UserAccount(f_name, m_name, l_name, u_name, u_email, l_password,)
     return newUserAccount
+
+def saveUserAccount(userAccount):
+
+    userAccount.saveUserAcc()
+
+def displayUserAccount():
+
+    return UserAccount.displayUser()
+
+    
