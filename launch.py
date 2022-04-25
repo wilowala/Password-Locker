9b8d0261ef720866cdeca21d82e90ed7554f5cf7
William Owalla <william.owalla@student.moringaschool.com>
2022-04-25 11:33:23 +0300

    initiate create user account method

--- launch.py
+++ launch.py
@@ -1,2 +1,7 @@
 from user import UserAccount
 from  credentials import  UserCredentials
+
+def createNewUserAccount(f_name, m_name, l_name, u_name, u_email, l_password):
+    
+    newUserAccount = UserAccount(f_name, m_name, l_name, u_name, u_email, l_password,)
+    return newUserAccount
